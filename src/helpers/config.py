dc462f6e1cb93ffb049b2b033d27c1eb002bbfa8
Mariam Omar <mo4473398@gmail.com>
2026-04-08 23:57:25 +0200

    update NLPController, route

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
 
     MONGODB_URL: str
     MONGODB_DATABASE: str
-    
+
     GENERATION_BACKEND: str
     EMBEDDING_BACKEND: str
 
@@ -26,11 +26,13 @@ class Settings(BaseSettings):
     INPUT_DAFAULT_MAX_CHARACTERS: int = None
     GENERATION_DAFAULT_MAX_TOKENS: int = None
     GENERATION_DAFAULT_TEMPERATURE: float = None
-    
-    
+
     VECTOR_DB_BACKEND : str
     VECTOR_DB_PATH : str
     VECTOR_DB_DISTANCE_METHOD: str = None
+
+    PRIMARY_LANG: str = "en"
+    DEFAULT_LANG: str = "en"
 
     class Config:
         env_file = ".env"
--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -26,3 +26,8 @@ class DataChunk(BaseModel):
                 "unique": False
             }
         ]
+    
+class RetrievedDocument(BaseModel):
+    text: str
+    score: float 
+    
